1924eef78e2681622af59de3b28eee3c95f52938
Sean Devine <sdevine815@gmail.com>
2025-05-13 08:54:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,6 @@
-import struct
-import random
-import math
+from network import NeuralNetwork
+from mnist_load import load_mnist_images, load_mnist_labels
 
-# ----------- STEP 1: Load MNIST files manually -----------
-
-def load_mnist_images(filename):
-
-  #opens file with 'rb': read binary
-  with open(filename, 'rb') as f:
-
-    #uses struct to unpack binary data, takes in 4 signed integers, starts reading at 16th byte
-    magic, num, rows, cols = struct.unpack(">IIII", f.read(16))
-    images = []
-
-    #reads in the binary, converts to integer, divides by 255 to make value between 0 and 1
-    for _ in range(num):
-      img = [ord(f.read(1)) / 255 for _ in range(rows * cols)]
-      images.append(img)
-      return images
-
-def load_mnist_labels(filename):
-  with open(filename, 'rb') as f:
-
-    #reads in 2 signed integers starting at the 8th byte
-    magic, num = struct.unpack(">II", f.read(8))
-    
-    #reads in a label which corresponds to the correct label for the images
-    labels = [ord(f.read(1)) for _ in range(num)]
-    return labels
-
-# ----------- STEP 2: Define Activation Functions -----------
-
-#sigmoid function represented by 1 / ( 1 + e^-x )
-def sigmoid(x):
-  return 1.0 / (1.0 + math.exp(-x))
-
-#derivatve of the sigmoid function
-def sigmoid_derivative(x):
-  sx = sigmoid(x)
-  return sx * (1 - sx)
-
-# ----------- STEP 4: Train and Evaluate -----------
-
-# You must download these files from the MNIST website:
 # t10k-images.idx3-ubyte, t10k-labels.idx1-ubyte, train-images.idx3-ubyte, train-labels.idx1-ubyte
 train_images = load_mnist_images("train-images.idx3-ubyte")
 train_labels = load_mnist_labels("train-labels.idx1-ubyte")
