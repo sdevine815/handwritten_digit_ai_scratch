fd1d3492aaadf3e494ddeaca44de6dd969494783
Sean Devine <sdevine815@gmail.com>
2025-05-13 08:57:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ train_labels = load_mnist_labels("train-labels.idx1-ubyte")
 test_images = load_mnist_images("t10k-images.idx3-ubyte")
 test_labels = load_mnist_labels("t10k-labels.idx1-ubyte")
 
+#784 input layer 28**2
+#32 hidden layer: arbitrary choice to avoid overfitting
+#10 output layer: corresponding to 0-9 digits
 nn = NeuralNetwork(784, 32, 10)
-nn.train(train_images[:1000], train_labels[:1000], epochs=5, lr=0.1) # using 1k samples for speed
+nn.train(train_images[:1000], train_labels[:1000], epochs=5, lr=0.1)
 
-# Test accuracy
+#Accuracy Demonstration
 correct = 0
 for i in range(100):
   prediction = nn.predict(test_images[i])
